0f7e902881fb33a4394b6b65936f11f392887f1f
Raaz <raazkatwal5@gmail.com>
2024-11-29 22:26:31 +0545

    created a chat app

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,13 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h1 class="font-bold text-4xl tracking-wide">Click on a User to Chat:</h1>
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @foreach ($users as $user)
+                    <p class="text-3xl my-4">
+                            <a href="{{ route('chat', $user->id) }}" class="drag-none">{{ $user->name }} </a>
+                    </p>
+                    @endforeach
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/dashboard', function (){
+    $users = User::where('id', '!=', auth()->user()->id)->get();
+    return view('dashboard', [
+        'users' => $users
+    ]);
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/chat/{id}', function (string $id){
+    return view('chat', [
+        'id' => $id
+    ]);
+})->middleware(['auth', 'verified'])->name('chat');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
